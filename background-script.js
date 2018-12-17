a648ede0cbeee3e22d403b813459e4b7dddc7e0f
Joe.Kalb <kalb.joseph@gmail.com>
2018-12-16 18:26:31 -0800

    second commit

--- background-script.js
+++ background-script.js
@@ -1,1 +1,33 @@
-console.log("this is from background-script.js");
+// this workds
+let button = document.createElement("button");
+const body = document.getElementsByTagName("body")[0];
+button.innerHTML = "new button";
+document.getElementsByTagName("body")[0].appendChild(button);
+
+// this works now!
+let title = document.getElementById("title");
+title.innerHTML = "3";
+
+console.log("hi kate") // very important part
+
+let word = {
+  "answer":"",
+  "wordCount":0,
+  "lettersCount":{}
+};
+
+function startGame() {
+  let wordOrigin = document.getElementById('wordInput').value.trim().toUpperCase();
+  document.getElementById('title').innerHTML = wordOrigin;
+
+  word.answer = wordOrigin;
+  word.wordCount = wordOrigin.split(" ").length;
+
+  wordOrigin.split("").forEach((el) => {
+    (word.lettersCount[el]) ? word.lettersCount[el] += 1 :
+      word.lettersCount[el] = 1;
+  });
+
+  //  'My guesses are a and B'.match(/\b\w\b/gi) // looks for first single letter in a message
+  console.log(word)
+}
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log(document.body.getElementsByClassName("chat-line__message"));
+let chat = document.getElementsByClassName("chat-list__lines tw-flex-grow-1 scrollable-area")[0];
+let textArea = document.getElementsByTagName("textarea")[0];
+
+let messages = [];
+let currentMessage = "";
+
+chat.addEventListener("DOMNodeInserted", function(event){
+  message = event.srcElement.innerText.split(':', 2);
+  messages.push({
+    "username": message[0],
+    "message": message[1].trim()
+  });
+  changeTextArea(message[1].trim());
+}, false);
+
+function changeTextArea(input){
+  currentMessage = textArea.value;
+  textArea.value = input;
+  console.log("Previous Message: ".concat(currentMessage));
+}
